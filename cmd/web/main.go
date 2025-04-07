7ad16f4e4bc2037e540e78c66d4986e91809606e
jwe4 <jeweaver@gmail.com>
2025-04-06 21:50:53 -0600

    initial

---
+++ cmd/web/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type application struct{}
+
+func main() {
+	// setup up an app config
+	app := application{}
+
+	// get application routes
+	mux := app.routes()
+	// print out a message
+	log.Println("Starting server on port 8080")
+	// start the server
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
